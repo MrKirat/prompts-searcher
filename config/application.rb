e46899915b480fac72ee08e211e9c008e8c88af4
taras <tviyatyk@gmail.com>
2023-08-25 13:15:01 +0300

    setup rspec

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module PromptsSearcher
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # rspec configuration
+    config.generators.test_framework = :rspec 
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
